7aa693e7cdd3f7aadeb19b789adb2f2cb1d099c7
GaelicGrime <will.angus.blaylock@gmail.com>
2022-07-06 19:02:08 -0600

    Updated emptyFont.py.

--- fonts/emptyFont.py
+++ fonts/emptyFont.py
@@ -5,7 +5,10 @@ BITS_TALL = 24
 FONT_NAME = "font15x24"
 HEADER_NAME = "FONT15x24_H"
 START_CHR = " "
-# !"#$ %&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~" "
+# !"#$ %&'()*+,-./
+# 0123456789:;<=>?
+# @ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_
+# `abcdefghijklmnopqrstuvwxyz{|}~°
 
 CHARACTERS = [
     [  # <space>
